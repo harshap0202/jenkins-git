6b66b435da106a3e41f14d90ffcd6e1d19a9ae23
harshap0202 <02harshpatil@gmail.com>
2023-04-26 11:55:00 +0530

    first

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello 
+{
+	public static void main(String []args) 
+	{
+		System.out.println("Obi-Van Kenobi : Hello There");
+		System.out.println("Grievous : General kenobi");
+	}
+}
